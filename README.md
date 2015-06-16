a8f431bc00aca665d3c06ee32be9ca9b2a7a1e34
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-06-16 09:46:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Sale Simplification
+==================
+
+Simplifies sale views and pipeline
+
+
+Features
+========
+* Cleans up the quotation form
+* Cleans up the sale form
+* Unifies sale quotation and sale order form syntax
---
+++ __openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2015- Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Sale Simplification',
+    'category': 'Sale',
+    'version': '0.1',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'website': 'http://www.tawasta.fi',    
+    'depends': ['sale'],
+    'description': '''
+Sale Simplification
+==================
+
+Simplifies sale views and pipeline
+
+
+Features
+========
+* Cleans up the quotation form
+* Cleans up the sale form
+* Unifies sale quotation and sale order form syntax
+''',
+    'data': [
+
+    ],
+}
