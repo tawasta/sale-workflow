259a476c55c3d8b872d94c4f846dee905ac53c03
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-07-08 10:20:17 +0300

    Refactor to follow PEP8

--- sale_order.py
+++ sale_order.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from openerp import models, api, fields
+from openerp import models, fields
+
 
 class SaleOrder(models.Model):
-    
+
     _inherit = 'sale.order'
-    
+
     show_all = fields.Boolean('Show all fields')
