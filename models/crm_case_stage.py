fc4c05068596833c68b7496055271b1d27c72a53
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-07-10 12:56:00 +0300

    Add a sale action setter

--- models/crm_case_stage.py
+++ models/crm_case_stage.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
+from openerp import models, api, fields
 
 
 class CrmCaseStage(models.Model):
@@ -7,12 +7,24 @@ class CrmCaseStage(models.Model):
     _inherit = 'crm.case.stage'
 
     sale_action = fields.Selection(selection='_get_sale_actions',
+                                   inverse='_set_sale_action',
                                    string='Sale action')
 
     def _get_sale_actions(self):
         sale_actions = (
             ('send_quotation', 'Send quotation'),
-            ('confirm_sale', 'Confirm quotation'),
+            ('opportunity_won', 'Opportunity won'),
         )
 
         return sale_actions
+
+    @api.one
+    def _set_sale_action(self):
+        # Unsets any duplicate action variables
+
+        records = self.search([('sale_action', '=', self.sale_action),
+                               ('sale_action', '!=', 'False'),
+                               ('id', '!=', self.id)])
+
+        for record in records:
+            record.sale_action = False
