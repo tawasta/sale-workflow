828f083fc918f1755693aa269909a6defab7a5c4
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-07-13 13:45:27 +0300

    Add a method to update opportunity stage when an order is confirmed

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 from openerp import models, api
 
+import logging
+_logger = logging.getLogger(__name__)
+
 
 class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
+
+    @api.multi
+    def write(self, vals):
+        ''' Extends default write to update opportunity as the
+        sale progresses '''
+
+        if 'state' in vals and vals['state'] == 'manual' and self.lead_id:
+            # Sale is confirmed. Mark the opportunity as won
+            self.lead_id.case_mark_won()
+
+        return super(SaleOrder, self).write(vals)
