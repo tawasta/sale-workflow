4c41c80660a181b6a1d25e8c225d8acfec4c6f1d
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-10 10:43:36 +0300

    Move sale order model

---
+++ models/sale_order.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    customer_contact = fields.Many2one('res.partner', "Customer's contact"),
