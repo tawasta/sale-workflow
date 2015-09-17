871ff7f19789942b691250b57ffcf0f2e80543f1
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-17 09:00:29 +0300

    Add domain filter

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
-    customer_contact = fields.Many2one('res.partner', "Customer's contact")
+    customer_contact = fields.Many2one(
+        'res.partner', "Customers contact",
+    )
+
+    @api.onchange('partner_invoice_id')
+    def onchange_partner_invoice_id(self):
+        self.customer_contact = self.env['res.partner'].search([
+            ('parent_id', '=', self.partner_id.id),
+        ], limit=1)
