b7de082416f569b738d27fdff545f6ed8d65d959
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-10-07 09:50:14 +0300

    Improve the onchange function

--- models/sale_order.py
+++ models/sale_order.py
@@ -37,18 +37,10 @@ class SaleOrder(models.Model):
         )
 
     # 3. Default methods
-    # def _default_name(self):
-
 
     # 4. compute and search fields, in the same order that fields declaration
-    # @api.multi
-    # @api.depends('seats_max', 'registration_ids.state')
-    # def _compute_seats(self):
-   
 
     # 5. Constraints and onchanges
-    # @api.constrains('seats_max', 'seats_available')
-    # def _check_seats_limit(self):
       
     @api.onchange('internal_sale')
     @api.depends('name', 'project_id')
@@ -60,19 +52,14 @@ class SaleOrder(models.Model):
             vals['parent_id'] = self.project_id.sudo().search([('code','=','INTSAL')]).id
             vals['partner_id'] = self.partner_id.id
 
-            analytic_account = self.project_id.create(vals)
+            # This thing is broken
+            analytic_account = self.project_id.with_context(partner_id = vals['partner_id']).create(vals)
             
             self.project_id = analytic_account.id
 
     # 6. CRUD methods
-    # def create(self):
-
-
+    
     # 7. Action methods
-    # @api.multi
-    # def action_validate(self):
-    #     self.ensure_one()
-
+    
     # 8. Business methods
-    # def mail_user_confirm(self):
     
