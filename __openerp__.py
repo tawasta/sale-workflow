1611e20b35b5b59aaf72b351c012e7b89c1c35d5
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-10-30 16:43:46 +0200

    Improve descriptor, readme, and module structure

--- __openerp__.py
+++ __openerp__.py
@@ -1,37 +1,37 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#   Copyright (c) 2015- Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#    Author: Jarmo Kortetjärvi
+#    Copyright 2015 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
 #
 ##############################################################################
+
 {
     'name': 'Sale Simplification',
+    'version': '8.0.0.1.1',
     'category': 'Sale',
-    'version': '0.1',
+    'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
-    'website': 'http://www.tawasta.fi',    
-    'depends': ['sale'],
-    'description': '''
-Sale Simplification
-==================
-
-Simplifies sale views and pipeline
-
-
-Features
-========
-* Cleans up the quotation form
-* Cleans up the sale form
-* Unifies sale quotation and sale order form syntax
-''',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'sale',
+        'sale_stock',
+    ],
     'data': [
         'view/sale_quotation_form.xml'
     ],
