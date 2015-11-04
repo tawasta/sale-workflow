d0f3e8b2ce1e719a481f8485c250bc6cff933c57
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-11-04 19:09:55 +0200

    Add a sale process simplification

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,12 +1,39 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields, api
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from openerp import api, fields, models
+
+# 4. Imports from Odoo modules (rarely, and only if necessary):
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
 
 
 class SaleOrder(models.Model):
-
+    # 1. Private attributes
     _inherit = 'sale.order'
 
+    _FIELD_STATES = {
+        'draft': [('readonly', False)],
+        'sent': [('readonly', False)],
+        'manual': [('readonly', False)],
+    }
+
+    # 2. Fields declaration
     show_all = fields.Boolean(
         'Show all fields',
         help="Some of the lesser used fields are hidden by default"
     )
+
+    # Allow modifying fields until the invoice is created
+    partner_id = fields.Many2one(states=_FIELD_STATES)
+    partner_invoice_id = fields.Many2one(states=_FIELD_STATES)
+    partner_shipping_id = fields.Many2one(states=_FIELD_STATES)
+
+    date_order = fields.Datetime(states=_FIELD_STATES)
+    order_line = fields.One2many(states=_FIELD_STATES)
---
+++ models/sale_order_line.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from openerp import api, fields, models
+
+# 4. Imports from Odoo modules (rarely, and only if necessary):
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class SaleOrderLine(models.Model):
+    # 1. Private attributes
+    _inherit = 'sale.order.line'
+
+    _FIELD_STATES = {
+        'draft': [('readonly', False)],
+        'confirmed': [('readonly', False)],
+    }
+
+    # 2. Fields declaration
+
+    # Allow modifying fields until the invoice is created
+    product_id = fields.Many2one(states=_FIELD_STATES)
+    name = fields.Text(states=_FIELD_STATES)
+    price_unit = fields.Float(states=_FIELD_STATES)
+    tax_id = fields.Many2many(states=_FIELD_STATES)
+    product_uom_qty = fields.Float(states=_FIELD_STATES)
+    product_uom = fields.Many2one(states=_FIELD_STATES)
+    product_uos_qty = fields.Float(states=_FIELD_STATES)
+    product_uos = fields.Many2one(states=_FIELD_STATES)
+    discount = fields.Float(states=_FIELD_STATES)
