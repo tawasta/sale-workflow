36557877f59828b1380530ec8462ed43470cfc10
Timo Talvitie <timo.talvitie@vizucom.com>
2017-10-18 15:14:25 +0300

    10.0 port of sale_order_description

--- sale_order_description/__openerp__.py
+++ sale_order_description/__openerp__.py
@@ -1,40 +1,44 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#   Copyright (c) 2015- Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2015- Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
 #
 ##############################################################################
+
 {
     'name': 'Sale Order Description',
-    'category': 'Sale',
-    'version': '8.0.1.0.1',
-    'author': 'Oy Tawasta Technologies Ltd.',
+    'summary': 'Adds a description (an internal note) to sale order',
+    'version': '10.0.1.0.0',
+    'category': 'Sales',
     'website': 'http://www.tawasta.fi',
-    'installable': False,    
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
     'depends': [
         'sale',
     ],
-    'description': '''
-Sale Order Description
-======================
-
-Adds a description (an internal note) to sale order
-
-
-Features
-========
-* Adds description field
-* Adds description notebook page
-''',
     'data': [
         'views/sale_order.xml',
+    ],
+    'demo': [
     ],
 }
