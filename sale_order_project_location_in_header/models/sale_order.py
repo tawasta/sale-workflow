9a91ab2c2eb1ffb5f7cde549c454b73d830dcb55
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-01-24 13:29:49 +0200

    Add a new module for adding a stock location to sale order

---
+++ sale_order_project_location_in_header/models/sale_order.py
@@ -0,0 +1,31 @@
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    FIELD_STATES = {
+        'draft': [('readonly', False)],
+        'sent': [('readonly', False)],
+    }
+
+    stock_location_id = fields.Many2one(
+        comodel_name='stock.location',
+        string='Location',
+        default=lambda self: self._default_get_stock_location(),
+        states=FIELD_STATES,
+        readonly=True,
+    )
+
+    def _default_get_stock_location(self):
+        if self.project_id and self.project_id.default_location_id:
+            self.stock_location_id = self.project_id.default_location_id
+
+    @api.onchange('project_id')
+    @api.depends('project_id')
+    def onchange_project_id_update_stock_location(self):
+        for record in self:
+            if record.project_id and record.project_id.default_location_id:
+                record.stock_location_id = \
+                    record.project_id.default_location_id.id
