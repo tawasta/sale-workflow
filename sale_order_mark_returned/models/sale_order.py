582dbd2fe098e93727b70ea731f73630bf2ec67b
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-03-26 19:38:02 +0300

    Improve formatting

--- sale_order_mark_returned/models/sale_order.py
+++ sale_order_mark_returned/models/sale_order.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields, api, _, exceptions
+from odoo import models, fields
 
 
 class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
-    returned = fields.Boolean('Returned', help='''Products have been shipped but then returned by the customer.''')
+    returned = fields.Boolean(
+        string='Returned',
+        help='Products have been shipped but then returned by the customer.'
+    )
