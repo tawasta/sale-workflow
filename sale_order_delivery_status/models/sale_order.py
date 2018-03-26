307c36690651cb18b12a8ac0738b50d8bd714730
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-03-26 19:29:01 +0300

    Improve formatting

--- sale_order_delivery_status/models/sale_order.py
+++ sale_order_delivery_status/models/sale_order.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, api, fields, _
+from odoo import models, fields
 
 
 class SaleOrder(models.Model):
@@ -13,31 +13,31 @@ class SaleOrder(models.Model):
                    ('open', 'To Deliver'),
                    ('partial', 'Partially Delivered'),
                    ('done', 'Fully Delivered')],
-        compute='_get_delivery_status',
+        compute='_compute_delivery_status',
         string='Delivery Status')
 
     # 3. Default methods
 
     # 4. Compute and search fields, in the same order that fields declaration
-    def _get_delivery_status(self):
-        '''Calculates the order's delivery status by comparing the lines' 
+    def _compute_delivery_status(self):
+        '''Calculates the order's delivery status by comparing the lines'
         delivered quantities to ordered quantities. Service lines are ignored
         in the calculation'''
 
         for order in self:
-            if all([l.product_id.type == 'service' \
-                or l.product_uom_qty == 0 \
-                for l in order.order_line]):
+            if all([l.product_id.type == 'service'
+                    or l.product_uom_qty == 0
+                    for l in order.order_line]):
                 # All services or lines with 0 qties, nothing to deliver
                 order.delivery_status = 'none'
-            elif all([l.qty_delivered >= l.product_uom_qty for l \
-                in order.order_line \
-                if l.product_id.type != 'service']):
+            elif all([l.qty_delivered >= l.product_uom_qty for l
+                      in order.order_line
+                      if l.product_id.type != 'service']):
                 # All physical products have been delivered
                 order.delivery_status = 'done'
-            elif all([l.qty_delivered == 0 for l \
-                in order.order_line \
-                if l.product_id.type != 'service']):
+            elif all([l.qty_delivered == 0 for l
+                      in order.order_line
+                      if l.product_id.type != 'service']):
                 # No physical products have been delivered
                 order.delivery_status = 'open'
             else:
