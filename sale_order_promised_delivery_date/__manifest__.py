ab3814509abbd3b96b3e0259748ce711902cb033
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-03-27 21:36:36 +0300

    Improve compliance with core fields
    Switch to datetime format and reposition field on form

--- sale_order_promised_delivery_date/__manifest__.py
+++ sale_order_promised_delivery_date/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Sale Order Promised Delivery Date',
     'summary': 'Adds a new field for storing date of promised delivery',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'Sales',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
--- sale_order_promised_delivery_date/models/sale_order.py
+++ sale_order_promised_delivery_date/models/sale_order.py
@@ -6,7 +6,7 @@ class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
-    date_delivery_promised = fields.Date(
+    date_delivery_promised = fields.Datetime(
         string='Promised Delivery Date',
         readonly=True,
         states={
