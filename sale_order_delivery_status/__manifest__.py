100d2b6ed010c9dc3feebf28ada40b01f7d3c504
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-04-26 09:26:32 +0300

    Update i18n

--- sale_order_delivery_status/__manifest__.py
+++ sale_order_delivery_status/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Delivery Status for Sale Orders',
     'summary': 'Delivery information for Sales',
     'category': 'Sales',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
