776f5d6d2dd03d666f83bdb44684959d390f22f0
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-05-17 19:44:16 +0300

    Add a new module for copying sale order lines
    This version cuts corners a lot: each copy will trigger a reload

---
+++ sale_order_line_copy/models/sale_order_line.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from odoo import api, fields, models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class SaleOrderLine(models.Model):
+
+    # 1. Private attributes
+    _inherit = 'sale.order.line'
+
+    # 2. Fields declaration
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+    def action_sale_order_line_copy(self):
+        ctx = dict(self.env.context)
+        self.ensure_one()
+
+        self.copy({'order_id': self.order_id.id})
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+
+    # 8. Business methods
