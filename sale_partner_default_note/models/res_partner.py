af0cfed37a17e9b5ca07d06033be0bbf454041a0
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-05-23 22:05:10 +0300

    Add a new module for default partner sale note

---
+++ sale_partner_default_note/models/res_partner.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from odoo import fields, models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class ResPartner(models.Model):
+
+    # 1. Private attributes
+    _inherit = 'res.partner'
+
+    # 2. Fields declaration
+    sale_note = fields.Char(
+        string='Sale note',
+        help='Default sale note',
+    )
+
+    # 3. Default methods
+
+    # 4. Compute and search fields
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
