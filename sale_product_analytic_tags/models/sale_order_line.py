cdd56479030876091fced852a3ca75635bca508a
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-02 20:47:44 +0300

    Add a new module for using product analytic tags on sale order lines

---
+++ sale_product_analytic_tags/models/sale_order_line.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    @api.onchange('product_id')
+    def onchange_product_id_update_analytic_tags(self):
+        for record in self:
+            if record.product_id and record.product_id.get_analytic_tags():
+                record.analytic_tag_ids = record.product_id.get_analytic_tags()
