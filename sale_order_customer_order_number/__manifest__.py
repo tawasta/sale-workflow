d5bba8ef3d2042bf486fcee530a531d6e3478a53
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-06-05 14:09:16 +0300

    Customer Order Number now goes to comment field on invoice
    Invoice no longer has a separate customer order number field. Instead, the
    CON from sale is placed in the comment (Terms and Conditions) field, so that
    it gets transferred when submitting invoices to external accounting software.
    
    The field is not yet deleted from invoice, just removed from the form view.

--- sale_order_customer_order_number/__manifest__.py
+++ sale_order_customer_order_number/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Customer Order Number for Sale Orders',
-    'summary': 'New field for order# given by customer',
-    'version': '10.0.1.0.0',
+    'summary': 'New field for order number provided by customer',
+    'version': '10.0.2.0.0',
     'category': 'Sales',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
@@ -37,7 +37,7 @@
         'sale',
     ],
     'data': [
-        'views/account_invoice.xml',
+        'views/sale_config_settings.xml',
         'views/sale_order.xml',
     ],
     'demo': [
---
+++ sale_order_customer_order_number/models/sale_config_settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class SaleConfigSettings(models.TransientModel):
+
+    _inherit = 'sale.config.settings'
+
+    customer_order_number_to_invoice = fields.Boolean(
+        related='company_id.customer_order_number_to_invoice',
+        string='Pass Customer Order Number to Invoice',
+    )
