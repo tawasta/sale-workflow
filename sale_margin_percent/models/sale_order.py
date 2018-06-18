f653d618e25b9765d78d65914f07f8062fb430ab
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-18 10:47:48 +0300

    Add new module for showing sale margins percent on sale order lines

---
+++ sale_margin_percent/models/sale_order.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    margin_percent = fields.Float(
+        string='Margin (%)',
+        digits=dp.get_precision('Margin'),
+        compute='_compute_margin_percent',
+    )
+
+    def _compute_margin_percent(self):
+        for record in self:
+            if record.amount_untaxed:
+                margin_percent = (record.margin / record.amount_untaxed) * 100
+                record.margin_percent = margin_percent
