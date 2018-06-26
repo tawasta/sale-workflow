63d9815f759a592f03a696f5d57bbcc8e3e05edd
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-26 12:09:49 +0300

    Add a new module for showing next delivery date on sale orders

---
+++ sale_order_next_delivery_date/models/sale_order.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from odoo import api, fields, models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class SaleOrder(models.Model):
+
+    # 1. Private attributes
+    _inherit = 'sale.order'
+
+    # 2. Fields declaration
+    next_delivery_date = fields.Date(
+        string='Next delivery',
+        compute='compute_next_delivery_date',
+        store=True,
+    )
+
+    # 3. Default methods
+
+    # 4. Compute and search fields
+    @api.depends('picking_ids')
+    def compute_next_delivery_date(self):
+        for record in self:
+            if record.picking_ids:
+                print record.picking_ids
+                next_delivery_ids = record.picking_ids.filtered(
+                    lambda r: r.state not in ('cancel', 'done', 'draft')
+                ).sorted(
+                    key=lambda r: r.min_date
+                )
+
+                print next_delivery_ids
+                for n in next_delivery_ids:
+                    print n.name
+                    print n.state
+
+                if next_delivery_ids:
+                    record.next_delivery_date = next_delivery_ids[0].min_date
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
