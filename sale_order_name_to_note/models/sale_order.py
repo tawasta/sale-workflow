dc3ce219929c1b699fe5aff76ed2cf2bbad92c80
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-27 11:58:43 +0300

    Add new module for adding sale name to sale note

---
+++ sale_order_name_to_note/models/sale_order.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+
+        # Add name (order reference) to sale note
+        for order in self:
+            if order.name:
+
+                reference = _('Our reference: %s' % order.name)
+
+                if order.note:
+                    order.note += '\n' + reference
+                else:
+                    order.note = reference
+
+        return res
