bed2222b46983253ed134aa8a38c6222c9f5b1c0
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-07-17 14:20:19 +0300

    Remove obsolete customizations

--- sale_order_comment_lines/__manifest__.py
+++ sale_order_comment_lines/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Allow using comment lines in sale orders',
     'summary': 'Allow using comment lines in sale orders',
     'category': 'Sale',
-    'version': '10.0.1.1.0',
+    'version': '10.0.1.2.0',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
