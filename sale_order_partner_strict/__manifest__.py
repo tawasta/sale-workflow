13759b2b6504239bd947516adff401793f320794
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-08-14 12:45:01 +0300

    Add a strict domain for partner_id

--- sale_order_partner_strict/__manifest__.py
+++ sale_order_partner_strict/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Strict domain filters for partners on sale',
     'summary': 'Only allow correct type and parent for addresses',
     'category': 'Sale',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
