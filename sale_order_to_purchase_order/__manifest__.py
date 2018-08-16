5008f0c0cda053b04421bca8d09dd4be54027d66
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-08-16 16:11:46 +0300

    Show purchase status on SO list view

--- sale_order_to_purchase_order/__manifest__.py
+++ sale_order_to_purchase_order/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Sale Order to Purchase Order',
     'summary': 'Button for creating a PO from SO, containing the same lines',
-    'version': '10.0.1.3.0',
+    'version': '10.0.1.4.0',
     'category': 'Sales',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
