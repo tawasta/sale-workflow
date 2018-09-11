d1bdeb2d7c00f67a300994a736a36e30ac178eae
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-09-11 13:36:52 +0300

    New module sale_order_confirmation_required_payment_Term

---
+++ sale_order_confirmation_required_payment_term/models/sale_order.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import models, exceptions, api, _
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_confirm(self):
+        for sale in self:
+            if not sale.partner_id.property_payment_term_id:
+                msg = _('Please fill in the customer payment terms for the '
+                        'customer %s.') \
+                    % sale.partner_id.name
+                raise exceptions.UserError(msg)
+
+        return super(SaleOrder, self).action_confirm()
