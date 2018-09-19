274cadd749d85e5978be00be432270a93f94358f
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-09-19 14:48:41 +0300

    Refactor formatting

--- sale_order_partner_disable_onchange/models/sale_order.py
+++ sale_order_partner_disable_onchange/models/sale_order.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
+from odoo import models, api
 
 
 class SaleOrder(models.Model):
-
     _inherit = 'sale.order'
 
     @api.multi
@@ -14,8 +13,8 @@ class SaleOrder(models.Model):
         # the dumb way and the change is undone after the method
 
         values = dict(
-            partner_invoice_id = self.partner_invoice_id,
-            partner_shipping_id = self.partner_shipping_id,
+            partner_invoice_id=self.partner_invoice_id,
+            partner_shipping_id=self.partner_shipping_id,
         )
 
         super(SaleOrder, self).onchange_partner_id()
