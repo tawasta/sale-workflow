697569d60c0f6714701ad4c790e55c9c666edd79
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-10-10 15:28:35 +0300

    Add a new module for sale order unlocking

---
+++ sale_order_unlock/models/sale_order.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api, _
+from odoo.exceptions import AccessError
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_unlock(self):
+        # Unlock sales
+
+        # Only allow sales manager to use this
+        if not self.env.user.has_group('sales_team.group_sale_manager'):
+            msg = _("Unlocking is only allowed for sales managers")
+            raise AccessError(msg)
+
+        for record in self:
+            record.state = 'sale'
