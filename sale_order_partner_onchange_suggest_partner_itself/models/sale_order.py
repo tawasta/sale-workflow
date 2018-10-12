265a34202a76c8e10c91e1c8071e896ce2de3d86
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-10-12 14:57:59 +0300

    New module sale_order_partner_onchange_suggest_partner_itself

---
+++ sale_order_partner_onchange_suggest_partner_itself/models/sale_order.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    @api.multi
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        # Override invoice and delivery address onchange and suggest the
+        # main customer for both fields
+
+        values = dict(
+            partner_invoice_id=self.partner_id \
+                and self.partner_id.id or False,
+            partner_shipping_id=self.partner_id \
+                and self.partner_id.id or False
+        )
+
+        super(SaleOrder, self).onchange_partner_id()
+        self.update(values)
