0c61d055536b8b75210b965889fcc1d1c8765d0c
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-10-23 13:29:54 +0300

    New module sale_order_to_purchase_order_sale_header

---
+++ sale_order_to_purchase_order_sale_header/models/purchase_order.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class PurchaseOrder(models.Model):
+
+    _inherit = 'purchase.order'
+
+    so_header_text = fields.Char(
+        related='sale_order_id.header_text',
+        string='Sale Order Header'
+    )
