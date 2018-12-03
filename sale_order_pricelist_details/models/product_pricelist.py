d7a7b5f1e8d0b1b4d2da6cbc483d0140fa187ffe
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-12-03 16:02:30 +0200

    [REF] Formatting

--- sale_order_pricelist_details/models/product_pricelist.py
+++ sale_order_pricelist_details/models/product_pricelist.py
@@ -8,7 +8,9 @@ class ProductPricelist(models.Model):
 
     @api.multi
     def action_pricelist_line_tree(self):
-        action = self.env.ref('sale_order_pricelist_details.action_pricelist_line_tree').read()[0]
+        action = self.env.ref(
+            'sale_order_pricelist_details.action_pricelist_line_tree')\
+            .read()[0]
         action['context'] = {
             'default_pricelist_id': self.id,
         }
