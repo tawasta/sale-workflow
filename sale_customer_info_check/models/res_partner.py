c18fd3128616ac97e088129b1e575b21f765b72e
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2018-12-04 11:43:25 +0200

    Modified structure to conforms Odoo rules

--- sale_customer_info_check/models/res_partner.py
+++ sale_customer_info_check/models/res_partner.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields
-import datetime
-from odoo.tools.translate import _
+
 
 class partner(models.Model):
 
     _inherit = 'res.partner'
-    
+
     info_verification_date = fields.Date('Partner info verified on')
-    
