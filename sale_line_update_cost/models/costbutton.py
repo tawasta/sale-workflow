5fcdfd907f4528ac0d0c462aacd5f26ea6ef62c8
difbit <timo.kekalainen@hotmail.com>
2019-03-13 13:32:42 +0200

    [FIX] Whitespaces removed and newlines added

--- sale_line_update_cost/models/costbutton.py
+++ sale_line_update_cost/models/costbutton.py
@@ -4,35 +4,32 @@ from odoo import api, fields, models
 
 
 class CostButton(models.Model):
-    
+
     _inherit = 'sale.order.line'
 
     new_cost = fields.Float(
-        string="Actual Cost", 
+        string="Actual Cost",
         compute="_compute_update_cost",
         help="Shows the current, actual cost behind the product."
         )
-
 
     @api.multi
     def _compute_update_cost(self):
 
         for record in self:
             product = record.product_id.with_context(
-            lang=record.order_id.partner_id.lang,
-            partner=record.order_id.partner_id.id,
-            quantity=record.product_uom_qty,
-            date=record.order_id.date_order,
-            pricelist=record.order_id.pricelist_id.id,
-            uom=record.product_uom.id
+                lang=record.order_id.partner_id.lang,
+                partner=record.order_id.partner_id.id,
+                quantity=record.product_uom_qty,
+                date=record.order_id.date_order,
+                pricelist=record.order_id.pricelist_id.id,
+                uom=record.product_uom.id
             )
 
-
-            res = record._get_purchase_price(record.order_id.pricelist_id, product, 
-             record.product_uom, record.order_id.date_order)
+            res = record._get_purchase_price(record.order_id.pricelist_id, product,
+                record.product_uom, record.order_id.date_order)
 
             record.new_cost = res['purchase_price']
-
 
     @api.multi
     def button_cost(self):
