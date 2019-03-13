c48d83a03f740f1a4d3f0335c7b34e23a132d1f5
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-03-13 10:28:41 +0200

    [REF] Formatting improvements

--- sale_customer_info_check/__manifest__.py
+++ sale_customer_info_check/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Customer Info Checker',
     'summary': 'Enables confirming multiple sales at once',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'CRM',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
