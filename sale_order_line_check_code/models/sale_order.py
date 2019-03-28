0f78de22dc090e03cc9f7df81dc4be6028edd3c5
difbit <timo.kekalainen@hotmail.com>
2019-03-28 16:37:37 +0200

    [FIX] Indentation fixed

--- sale_order_line_check_code/models/sale_order.py
+++ sale_order_line_check_code/models/sale_order.py
@@ -48,10 +48,14 @@ class SaleOrderLine(models.Model):
             nextline = ""
 
         if self.product_id.description_sale:
-            self.name = "%s%s%s" % (self.product_id.description_sale,
-            nextline, self.old_product_id)
+            self.name = "%s%s%s" % (
+                self.product_id.description_sale,
+                nextline, self.old_product_id
+            )
         else:
-            self.name = "%s%s%s" % (self.product_id.name, nextline,
-            self.old_product_id)
+            self.name = "%s%s%s" % (
+                self.product_id.name, nextline,
+                self.old_product_id
+            )
 
         return res
