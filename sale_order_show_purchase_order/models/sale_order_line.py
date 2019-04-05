6a6895cb6228d4e339ab911eac2ea570774e54de
difbit <timo.kekalainen@hotmail.com>
2019-04-05 14:01:03 +0300

    [ADD] A new module that links products on sale order lines to purchase orders

---
+++ sale_order_show_purchase_order/models/sale_order_line.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import api, fields, models
+
+ 
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+            
+    @api.multi
+    def _get_purchase_ids(self):       
+
+        group_list = []
+        purchase_list = []
+
+        purchases = self.purchase_order_ids.search([])
+
+        for record in self.order_line:
+            if record != False:
+                for line in self.order_line:
+                    if line.procurement_ids.ids != []:
+                        group_list.append(line.procurement_ids.group_id)
+
+            if group_list != []:
+                for group in group_list:
+                    print group
+                    for id_number in group.procurement_ids:
+                        if id_number.purchase_id:
+                            purchase_list.append(id_number.purchase_id.ids[0])
+            if purchase_list != []:
+                for i in purchase_list:
+                    self.purchase_order_ids \
+                    = [x for x in purchases.ids if x == i]
+                              
+    purchase_order_ids = fields.Many2many(
+        compute=_get_purchase_ids,
+        comodel_name='purchase.order',
+        string='Purchase Orders'
+        )
