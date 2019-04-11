4fd120dc060106fb4b7050cc5bdee8cbae43af6c
difbit <timo.kekalainen@hotmail.com>
2019-04-11 12:58:38 +0300

    [IMP] Removed whitespaces, print and added a condition

--- sale_order_show_purchase_order/models/sale_order_line.py
+++ sale_order_show_purchase_order/models/sale_order_line.py
@@ -3,13 +3,13 @@
 
 from odoo import api, fields, models
 
- 
+
 class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
-            
+
     @api.multi
-    def _get_purchase_ids(self):       
+    def _get_purchase_ids(self):
 
         group_list = []
         purchase_list = []
@@ -17,22 +17,23 @@ class SaleOrder(models.Model):
         purchases = self.purchase_order_ids.search([])
 
         for record in self.order_line:
-            if record != False:
+            if record is not False:
                 for line in self.order_line:
                     if line.procurement_ids.ids != []:
                         group_list.append(line.procurement_ids.group_id)
 
             if group_list != []:
+
                 for group in group_list:
-                    print group
                     for id_number in group.procurement_ids:
-                        if id_number.purchase_id:
+                        if id_number.purchase_id and id_number.purchase_id.ids:
                             purchase_list.append(id_number.purchase_id.ids[0])
+
             if purchase_list != []:
                 for i in purchase_list:
                     self.purchase_order_ids \
-                    = [x for x in purchases.ids if x == i]
-                              
+                        = [x for x in purchases.ids if x == i]
+
     purchase_order_ids = fields.Many2many(
         compute=_get_purchase_ids,
         comodel_name='purchase.order',
