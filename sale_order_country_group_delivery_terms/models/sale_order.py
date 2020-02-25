5244cf56d1f9eabe50b2697705fee0753f488293
retropikzel <retropikzel@mailbox.org>
2020-02-25 12:52:37 +0200

    [ADD] Pre-commit and module to migrate

---
+++ sale_order_country_group_delivery_terms/models/sale_order.py
@@ -0,0 +1,30 @@
+from odoo import _, api, models
+
+
+class SaleOrder(models.Model):
+
+    _inherit = "sale.order"
+
+    @api.multi
+    def action_confirm(self):
+        self._add_delivery_term()
+
+        return super(SaleOrder, self).action_confirm()
+
+    def _add_delivery_term(self):
+        for record in self:
+            shipping_address = record.partner_shipping_id or record.partner_id
+
+            country_groups = (
+                shipping_address.country_id
+                and shipping_address.country_id.country_group_ids
+            )
+
+            if country_groups:
+                delivery_terms = country_groups[0].delivery_terms
+                msg = "{}: {}".format(_("Delivery terms"), delivery_terms)
+
+                if record.note:
+                    record.note += "\n%s" % msg
+                else:
+                    record.note = msg
