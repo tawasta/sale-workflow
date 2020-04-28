e558c0895561558d678802c1866e6503618375ee
retropikzel <retropikzel@mailbox.org>
2020-04-28 08:12:57 +0300

    [FIX] Make field simple for now

--- sale_order_week_of_shipment/models/sale_order.py
+++ sale_order_week_of_shipment/models/sale_order.py
@@ -5,4 +5,4 @@ class SaleOrder(models.Model):
 
     _inherit = "sale.order"
 
-    week_of_shipment = fields.Integer(string="Week of shipment")
+    week_of_shipment = fields.Integer(string="Week of shipment", readonly=False)
