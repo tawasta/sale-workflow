73a55148db5288fe644f213cda2eb7b36042fe31
retropikzel <retropikzel@mailbox.org>
2020-05-04 10:03:16 +0300

    [FIX] Week of shipment

--- sale_order_week_of_shipment/models/sale_order.py
+++ sale_order_week_of_shipment/models/sale_order.py
@@ -1,5 +1,3 @@
-import datetime
-
 from odoo import fields, models
 
 
@@ -7,14 +5,6 @@ class SaleOrder(models.Model):
 
     _inherit = "sale.order"
 
-    def compute_default_week_of_shipment(self):
-        week_number = datetime.date(
-            self.expected_date.year, self.expected_date.month, self.expected_date.day
-        ).isocalendar()[1]
-        self.week_of_shipment = week_number
-
     week_of_shipment = fields.Integer(
-        string="Week of shipment",
-        readonly=False,
-        computed="compute_default_week_of_shipment",
+        string="Week of shipment", readonly=False, default=1
     )
