3f16395ae9494d50f0028be512cad8945b09785e
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-05-15 12:57:21 +0300

    [IMP] Rollback CRUD overrides (too invasive)

--- sale_order_customer_contact/__manifest__.py
+++ sale_order_customer_contact/__manifest__.py
@@ -19,7 +19,7 @@
 {
     "name": "Customer Contact for Sale Orders",
     "category": "Sales",
-    "version": "12.0.1.2.2",
+    "version": "12.0.1.3.0",
     "installable": True,
     "author": "Tawasta",
     "license": "AGPL-3",
--- sale_order_customer_contact/models/sale_order.py
+++ sale_order_customer_contact/models/sale_order.py
@@ -75,22 +75,3 @@ class SaleOrder(models.Model):
         )
         return invoice_vals
 
-    @api.model
-    def create(self, values):
-        if 'customer_contact_id' not in values:
-            values['customer_contact_id'] = values['partner_id']
-
-        res = super(SaleOrder, self).create(values)
-        res.onchange_partner()
-
-        return res
-
-    @api.multi
-    def write(self, values):
-        if 'partner_id' in values:
-            # Force using commercial partner as partner
-            partner = self.env['res.partner'].browse(values['partner_id'])
-            if partner.commercial_partner_id != partner:
-                values['partner_id'] = partner.commercial_partner_id.id
-
-        return super(SaleOrder, self).write(values)
