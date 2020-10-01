72feb07e77f410eb6201ddd777e32b1f372bbf54
difbit <timo.kekalainen@hotmail.com>
2020-10-01 09:26:23 +0300

    [IMP] Changed the _get_delivery_methods()-method which is used on website sale

--- sale_order_delivery_method_weight_constraint/models/sale_order.py
+++ sale_order_delivery_method_weight_constraint/models/sale_order.py
@@ -10,6 +10,22 @@ class SaleOrder(models.Model):
     max_weight = fields.Float(related="carrier_id.max_weight")
     min_weight = fields.Float(related="carrier_id.min_weight")
 
+    def _get_delivery_methods(self):
+    """This method is used on website sale"""
+        delivery_methods = super(SaleOrder, self)._get_delivery_methods()
+        total_delivery_weight = sum(
+            [x.product_id.weight * x.product_uom_qty for
+             x in self.order_line])
+
+        for delivery_method in delivery_methods:
+            if ((delivery_method.max_weight and
+                 delivery_method.max_weight < total_delivery_weight) or
+                (delivery_method.min_weight and
+                 delivery_method.min_weight > total_delivery_weight)):
+                delivery_methods -= delivery_method
+
+        return delivery_methods
+
     # Trigger computation if SO line's Quantity, Product
     # or Delivery method changes
     @api.depends('order_line.product_uom_qty', 'order_line.product_id',
