957fb2de41c854d4e0924d9791b29d135afdda07
Timo Kekäläinen <36134879+difbit@users.noreply.github.com>
2020-11-20 11:51:50 +0200

    [IMP] Improved assigning value (#202)

--- sale_order_year_of_shipment/models/sale_order.py
+++ sale_order_year_of_shipment/models/sale_order.py
@@ -7,20 +7,18 @@ class SaleOrder(models.Model):
 
     year_of_shipment = fields.Char(
         string="Year of shipment",
-        compute=lambda self: self._compute_delivery_year()
+        compute="_compute_delivery_year"
     )
 
-    @api.depends('commitment_date')
     def _compute_delivery_year(self):
         """Compute method for Year of shipment-field"""
         for sale in self:
-            # sudo() bypasses record rules
-            sale.year_of_shipment = sale.sudo().commitment_date and \
-                    str(sale.sudo().commitment_date.year)
+            sale.year_of_shipment = sale.commitment_date and \
+                    str(sale.commitment_date.year)
 
     @api.onchange("commitment_date")
     def onchange_shipment_year(self):
         """User-friendly Onchange method for Year of shipment-field"""
         for sale in self:
-            sale.sudo().year_of_shipment = sale.commitment_date and \
+            sale.year_of_shipment = sale.commitment_date and \
                     str(sale.commitment_date.year)
