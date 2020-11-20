296e3fc11e3236dff5448fd40dea77800333eb4c
Timo Kekäläinen <36134879+difbit@users.noreply.github.com>
2020-11-20 10:34:33 +0200

    12.0 dev year shipment tree (#201)
    * [IMP] Improved computing
    
    * [IMP] Year of shipment added to tree view

--- sale_order_year_of_shipment/models/sale_order.py
+++ sale_order_year_of_shipment/models/sale_order.py
@@ -7,15 +7,16 @@ class SaleOrder(models.Model):
 
     year_of_shipment = fields.Char(
         string="Year of shipment",
-        compute="_compute_delivery_year"
+        compute=lambda self: self._compute_delivery_year()
     )
 
+    @api.depends('commitment_date')
     def _compute_delivery_year(self):
         """Compute method for Year of shipment-field"""
         for sale in self:
             # sudo() bypasses record rules
-            sale.sudo().year_of_shipment = sale.commitment_date and \
-                    str(sale.commitment_date.year)
+            sale.year_of_shipment = sale.sudo().commitment_date and \
+                    str(sale.sudo().commitment_date.year)
 
     @api.onchange("commitment_date")
     def onchange_shipment_year(self):
