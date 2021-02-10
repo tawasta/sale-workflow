dc1d87fd73ce2dd753690715c03aaf0dfa052a24
difbit <timo.kekalainen@hotmail.com>
2021-02-10 10:07:13 +0200

    [IMP] Field set to translatable

--- sale_order_country_group_delivery_terms/models/res_country_group.py
+++ sale_order_country_group_delivery_terms/models/res_country_group.py
@@ -8,4 +8,4 @@ class CountryGroup(models.Model):
 
     sequence = fields.Integer()
     skip_delivery_terms = fields.Boolean(string="Skip Delivery terms")
-    delivery_terms = fields.Char(string="Delivery Terms")
+    delivery_terms = fields.Char(string="Delivery Terms", translate=True)
