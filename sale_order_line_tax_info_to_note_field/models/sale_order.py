999dc1893e8ec1b0c283791c5fa1cd4f8c189e63
difbit <timo.kekalainen@hotmail.com>
2021-02-10 10:09:45 +0200

    [IMP] Translate sale order note by customer's language

--- sale_order_line_tax_info_to_note_field/models/sale_order.py
+++ sale_order_line_tax_info_to_note_field/models/sale_order.py
@@ -11,11 +11,12 @@ class SaleOrder(models.Model):
 
         for order in self:
             note_lines = self.note.split('\n')
-            tax_info = self.env['account.tax'].search([]).filtered(
+            tax_info = self.env['account.tax'].with_context(
+                lang=order.partner_id.lang).search([]).filtered(
                 lambda tx: tx.sale_order_note).mapped('sale_order_note')
 
-            note_lines = [line for line in note_lines \
-                    if not any(word in line for word in tax_info)]
+            note_lines = [line for line in note_lines
+                          if not any(word in line for word in tax_info)]
 
             note_lines = '\n'.join(note_lines)
             order.note = note_lines
@@ -24,13 +25,14 @@ class SaleOrder(models.Model):
 
             if order.order_line:
                 for line in order.order_line:
-                    tax_note = line.tax_id.sale_order_note
+                    tax_note = line.with_context(
+                        lang=order.partner_id.lang).tax_id.sale_order_note
                     if tax_note and tax_note not in tax_info:
                         tax_info.append(tax_note)
 
             tax_info = '\n'.join(tax_info)
 
             if tax_info:
-                order.note = '{}{}{}'.format(note_lines,
-                    '\n' if note_lines else '', tax_info)
+                order.note = '{}{}{}'.format(
+                    note_lines, '\n' if note_lines else '', tax_info)
         return res
