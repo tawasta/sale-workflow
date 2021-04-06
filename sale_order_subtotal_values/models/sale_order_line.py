330666ce5a9a9b97e0c20da6fe5a0572dddf445d
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2021-04-06 16:44:40 +0300

    [FIX] Add super-call

--- sale_order_subtotal_values/models/sale_order_line.py
+++ sale_order_subtotal_values/models/sale_order_line.py
@@ -3,11 +3,20 @@ from odoo import api, models
 
 class SaleOrderLine(models.Model):
 
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
-    @api.depends('product_id', 'purchase_price', 'product_uom_qty', 'price_unit', 'price_subtotal')
+    @api.depends(
+        "product_id",
+        "purchase_price",
+        "product_uom_qty",
+        "price_unit",
+        "price_subtotal",
+    )
     def _product_margin(self):
+        super()._product_margin()
         for line in self:
             currency = line.order_id.pricelist_id.currency_id
             price = line.purchase_price
-            line.margin = currency.round(line.price_subtotal - (price * line.product_uom_qty))
+            line.margin = currency.round(
+                line.price_subtotal - (price * line.product_uom_qty)
+            )
