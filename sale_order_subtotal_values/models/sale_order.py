d64e83b4d491e06d98cec52216108dc1323c0da2
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2021-04-07 15:45:03 +0300

    [IMP] Less intrusive way of ensuring the subtotal calculation

---
+++ sale_order_subtotal_values/models/sale_order.py
@@ -0,0 +1,23 @@
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+
+    _inherit = "sale.order"
+
+    @api.model
+    def create(self, values):
+        res = super().create(values)
+        for sale in res:
+             sale.order_line._compute_amount()
+
+        return res
+
+    @api.multi
+    def write(self, values):
+        res = super().write(values)
+        if 'order_line' in values:
+            for sale in self:
+                sale.order_line._compute_amount()
+
+        return res
