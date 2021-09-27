e665735afac3e191f25a84f70f99e7e220556dff
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2021-09-27 13:46:34 +0300

    [IMP] Remove company limitation from SO line product

--- sale_multi_company/__manifest__.py
+++ sale_multi_company/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Website Sale Multi Company",
     "summary": "Allows selling multiple company products on a single sale",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Website",
     "website": "https://gitlab.com/tawasta/odoo/e-commerce",
     "author": "Tawasta",
@@ -33,5 +33,5 @@
         "sale_management",
         "stock",
     ],
-    "data": ["security/product_security.xml"],
+    "data": ["security/product_security.xml", "views/sale_order.xml"],
 }
