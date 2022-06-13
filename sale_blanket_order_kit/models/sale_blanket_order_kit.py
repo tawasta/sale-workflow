ef6e56cb19912fd4148a3a99b4ee8f5faf3a6e7f
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-06-13 15:52:01 +0300

    [ADD] New module sale_blanket_order_kit

---
+++ sale_blanket_order_kit/models/sale_blanket_order_kit.py
@@ -0,0 +1,22 @@
+from odoo import fields
+from odoo import models
+
+
+class SaleBlanketOrderKit(models.Model):
+    _name = "sale.blanket.order.kit"
+
+    order_id = fields.Many2one("sale.blanket.order", required=True, ondelete="cascade")
+
+    kit_id = fields.Many2one(
+        comodel_name="mrp.bom",
+        name="Kit",
+        domain=[("type", "=", "phantom")],
+        required=True,
+    )
+
+    original_uom_qty = fields.Float(
+        "Original quantity",
+        required=True,
+        default=1,
+        digits="Product Unit of Measure",
+    )
