7d2e11e7729ff6028532d73cb5cb3e9eb388799d
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-05-11 10:20:22 +0000

    [ADD] A new module: sale_order_note_by_comment_to_picking

---
+++ sale_order_note_by_comment_to_picking/models/stock_picking.py
@@ -0,0 +1,40 @@
+from odoo import _, api, models
+
+
+class StockPicking(models.Model):
+
+    _inherit = "stock.picking"
+
+    @api.model
+    def create(self, vals):
+        picking = super().create(vals)
+
+        # Yes, we search with origin-field which is not ideal
+        origin = vals.get("origin")
+        sale_id = self.env["sale.order"].sudo().search([("name", "=", origin)])
+
+        if sale_id:
+            # This is a link to a sale order
+            ref = "<a href=# data-oe-model=sale.order data-oe-id=%d>%s</a>" % (
+                sale_id.id,
+                sale_id.name,
+            )
+
+            display_msg = """<div style="color: green;">
+                Note by: {}
+                </div>
+                <div style="color: green;">
+                This transfer has been created from: {}
+                </div>
+                """.format(
+                sale_id.user_id.name, ref
+            )
+
+            picking.sudo().message_post(
+                message_type="comment",
+                subject=_("Sale Order created"),
+                body=display_msg,
+                notify_by_email=False,
+            )
+
+        return picking
