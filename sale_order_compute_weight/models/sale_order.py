d600fd01f55cc2881307a7e356c26336904e7167
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-08-29 09:52:11 +0000

    [MIG] Migration of sale_order_weight module from 12.0 version

---
+++ sale_order_compute_weight/models/sale_order.py
@@ -0,0 +1,20 @@
+from odoo import fields, models
+
+from odoo.addons import decimal_precision as dp
+
+
+class SaleOrder(models.Model):
+
+    _inherit = "sale.order"
+
+    weight = fields.Float(
+        "Weight", digits=dp.get_precision("Stock Weight"), compute="_compute_weight"
+    )
+
+    def _compute_weight(self):
+        for record in self:
+            weight = 0
+            for line in record.order_line:
+                weight += line.weight
+
+            record.weight = weight
