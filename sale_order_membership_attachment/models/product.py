3a6531aeb87d0f47470233ca71c4ff22a8a17258
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-03-06 13:56:09 +0200

    Modified field

--- sale_order_membership_attachment/models/product.py
+++ sale_order_membership_attachment/models/product.py
@@ -1,8 +1,12 @@
 from odoo import fields, models
 
-
 class ProductTemplate(models.Model):
-
     _inherit = "product.template"
 
-    membership_attachment_ids = fields.Many2many("ir.attachment", string="Attachments")
+    membership_attachment_ids = fields.Many2many(
+        comodel_name="ir.attachment",
+        relation="product_template_membership_attachment_rel",  # Uniikki relation-taulun nimi
+        column1="product_template_id",
+        column2="attachment_id",
+        string="Membership Attachments"
+    )
