253654daba1e9d7d1c651929450199fbf5e813c1
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-10-08 10:45:00 +0300

    [IMP] Grant pricelist to user automatically

--- sale_order_product_pricelist/models/sale_order_line.py
+++ sale_order_product_pricelist/models/sale_order_line.py
@@ -4,6 +4,21 @@ from odoo import api, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+    @api.model
+    def create(self, vals):
+        res = super().create(vals)
+
+        product_pricelist = res.product_id and res.product_id.pricelist_id
+        # grant_pricelist = product_pricelist and res.order_id.pricelist_id != product_pricelist
+        # Granting a pricelist is disabled, as it would allow getting all items with the selected pricelist
+        grant_pricelist = False
+
+        if grant_pricelist:
+            # Product adds a pricelist, but it hasn't been added yet
+            res.order_id.pricelist_id = product_pricelist
+
+        return res
+
     def _compute_pricelist_item_id(self):
         res = super()._compute_pricelist_item_id()
 
@@ -19,7 +34,7 @@ class SaleOrderLine(models.Model):
                 if pricelist_id == line.order_id.pricelist_id:
                     # If order is already using this pricelist, do nothing
                     continue
-                # If products in order add a pricelist, use that for subsequential lines
+                # If products in order add a pricelist, use that for subsequent lines
                 line.pricelist_item_id = pricelist_id._get_product_rule(
                     line.product_id,
                     quantity=line.product_uom_qty or 1.0,
