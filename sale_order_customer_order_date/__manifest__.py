171a8efed67c69efe38123daaf8ef9470cad371a
difbit <timo.kekalainen@tawasta.fi>
2025-04-10 13:29:51 +0300

    [IMP] Improvements to modules to align header fields

--- sale_order_customer_order_date/__manifest__.py
+++ sale_order_customer_order_date/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Sale Order Customer Order Date",
     "summary": "Customer order date field to sale order",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Sales",
     "website": "https://github.com/tawasta/sale-workflow",
     "author": "Futural",
