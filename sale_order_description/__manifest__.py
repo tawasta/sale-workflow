52a1a4528205567d0428bea98ac50fca50cc9a03
Timo Talvitie <timo.talvitie@tawasta.fi>
2025-04-30 16:59:07 +0300

    [IMP] add name attribute to notebook page for easier inheriting

--- sale_order_description/__manifest__.py
+++ sale_order_description/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Sale Order Description",
     "summary": "Adds a description (an internal note) to sale order",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Sales",
     "website": "https://github.com/tawasta/sale-workflow",
     "author": "Futural",
