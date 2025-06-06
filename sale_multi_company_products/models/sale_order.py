062a3d310716157b359a74764bae301caa671e07
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-06-06 10:25:24 +0300

    [IMP] - modified functiond and removed functions

--- sale_multi_company_products/models/sale_order.py
+++ sale_multi_company_products/models/sale_order.py
@@ -108,33 +108,6 @@ class SaleOrder(models.Model):
 
         return res
 
-    def _get_program_domain(self):
-        """
-        Returns the base domain that all programs have to comply to.
-        """
-        self.ensure_one()
-        today = fields.Date.context_today(self)
-        return [('active', '=', True), ('sale_ok', '=', True),
-                *self.env['loyalty.program']._check_company_domain([self.company_id.id, self.company_id.parent_id.id]),
-                '|', ('pricelist_ids', '=', False), ('pricelist_ids', 'in', [self.pricelist_id.id]),
-                '|', ('date_from', '=', False), ('date_from', '<=', today),
-                '|', ('date_to', '=', False), ('date_to', '>=', today)]
-
-    def _get_trigger_domain(self):
-        """
-        Returns the base domain that all triggers have to comply to.
-        """
-        self.ensure_one()
-        today = fields.Date.context_today(self)
-        return [('active', '=', True), ('program_id.sale_ok', '=', True),
-                *self.env['loyalty.program']._check_company_domain([self.company_id.id, self.company_id.parent_id.id]),
-                '|', ('program_id.pricelist_ids', '=', False),
-                     ('program_id.pricelist_ids', 'in', [self.pricelist_id.id]),
-                '|', ('program_id.date_from', '=', False), ('program_id.date_from', '<=', today),
-                '|', ('program_id.date_to', '=', False), ('program_id.date_to', '>=', today)]
-
-
-
     def _try_apply_code(self, code):
         self.ensure_one()
 
