b953d4834bb9117dd58541d9e45b5c37c6f6191a
Jarmo Kortetjärvi <jarmo.kortetjarvi@futural.fi>
2025-08-26 13:30:37 +0300

    [ADD] New module sale_order_invoice_auto_confirm
    Automatically confirm SO invoices if all products are set to be auto-confirmed.

---
+++ sale_order_invoice_auto_confirm/__manifest__.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+#    Author: Futural Oy
+#    Copyright 2025 Futural Oy (https://futural.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Sale order invoice auto confirm",
+    "summary": "Confirm SO invoices if all products are set to be auto-confirmed",
+    "version": "17.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://github.com/tawasta/sale-workflow",
+    "author": "Futural",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "auto_install": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "product",
+        "sale",
+    ],
+    "data": [
+        "views/product_template.xml",
+        "views/sale_order.xml",
+    ],
+    "assets": {},
+    "demo": [],
+}
