d606dbfdf28bde51bf89b4230d7fe99c7a8fc3a3
Jarmo Kortetjärvi <jarmo.kortetjarvi@futural.fi>
2025-09-17 11:48:23 +0300

    [IMP] Allow manual disable for auto-confirm

--- sale_order_invoice_auto_confirm/models/sale_order.py
+++ sale_order_invoice_auto_confirm/models/sale_order.py
@@ -7,6 +7,7 @@ class SaleOrder(models.Model):
     auto_confirm_invoice = fields.Boolean(
         string="Auto Confirm Invoices",
         compute="_compute_auto_confirm_invoice",
+        store=True,
     )
 
     @api.depends("order_line.product_id")
@@ -16,6 +17,9 @@ class SaleOrder(models.Model):
             order.auto_confirm_invoice = bool(lines) and all(
                 line.product_id.auto_confirm_sale_invoice for line in lines
             )
+
+    def action_disable_auto_confirm(self):
+        self.write({"auto_confirm_invoice": False})
 
     def _create_invoices(self, grouped=False, final=False, date=None):
         invoices = super()._create_invoices(grouped=grouped, final=final, date=date)
