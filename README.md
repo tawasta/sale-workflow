d339c363fd0bcae9eacef8b9cf69043a36723171
difbit <timo.kekalainen@tawasta.fi>
2025-10-29 10:06:58 +0200

    [ADD] A new module: sale_order_button_confirm_pickings_and_create_invoice. Confirm sale, invoice and picking with a new button

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ addon | version | maintainers | summary
 [sale_order_additional_note](sale_order_additional_note/) | 17.0.1.0.1 |  | Adds Html-type field to Sale Order, which is shown on its PDF print
 [sale_order_address_details](sale_order_address_details/) | 17.0.1.0.2 |  | Sale order address details
 [sale_order_analytic_distribution_required_upon_invoice](sale_order_analytic_distribution_required_upon_invoice/) | 17.0.1.0.0 |  | Creating an invoice from SO is not possible if analytic distributions are not set on lines
+[sale_order_button_confirm_pickings_and_create_invoice](sale_order_button_confirm_pickings_and_create_invoice/) | 17.0.1.0.0 |  | Add a button to confirm sale, invoice and picking
 [sale_order_cancel_delete_purchase_line](sale_order_cancel_delete_purchase_line/) | 17.0.1.1.0 |  | Cancelling a sale order deletes its related purchase lines
 [sale_order_client_order_ref_form_header](sale_order_client_order_ref_form_header/) | 17.0.1.0.0 |  | Reposition reference field on SO form
 [sale_order_country_group_delivery_terms](sale_order_country_group_delivery_terms/) | 17.0.1.0.2 |  | Add note from country groups to SO when confirming the sale
---
+++ sale_order_button_confirm_pickings_and_create_invoice/models/sale_order.py
@@ -0,0 +1,51 @@
+from odoo import fields, models, _
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def validate_pickings_then_create_and_post_invoice(self):
+        """@Return:
+        1. Verify and Confirm SO.
+        2. Delivery order sets to done.
+        3. Create an invoice.
+        4. Validate an invoice.
+        """
+        self.action_confirm()
+        invoices = []
+        for order in self:
+            for picking in order.picking_ids:
+                picking.action_assign()
+                picking.action_confirm()
+                picking.button_validate()
+
+            order._create_invoices()
+            for invoice in order.invoice_ids:
+                invoice.action_post()
+
+        # Display invoice view after confirm sales order.
+        invoices.extend(order.invoice_ids.ids)
+        if invoices:
+            action = {
+                "name": _("Invoices"),
+                "domain": [("id", "in", invoices)],
+                "res_model": "account.move",
+                "view_mode": "tree,form",
+                "type": "ir.actions.act_window",
+                "views": [
+                    (self.env.ref("account.view_move_tree").id, "tree"),
+                    (False, "form"),
+                ],
+            }
+            if len(invoices) == 1:
+                action = {
+                    "name": _("Invoices"),
+                    "res_id": invoices[0],
+                    "res_model": "account.move",
+                    "view_mode": "form",
+                    "view_type": "tree",
+                    "type": "ir.actions.act_window",
+                    "views": [(self.env.ref("account.view_move_form").id, "form")],
+                }
+            return action
+        return True
